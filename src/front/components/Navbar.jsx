177fb78e693d66686bae2a4006d9655c44435506
Bastián Cartagena <b.cartagena.paredes@gmail.com>
2025-07-06 18:10:33 +0000

    modified file

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,19 +1,37 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import  useGlobalReducer  from "../hooks/useGlobalReducer"; 
 
 export const Navbar = () => {
+    const { store, dispatch } = useGlobalReducer(); 
+    const navigate = useNavigate();
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+    const handleLogout = () => {
+        dispatch({ type: "CLEAR_AUTH" });
+        navigate("/login"); 
+    };
+
+    return (
+        <nav className="navbar navbar-light bg-light mb-3">
+            <Link to="/">
+                <span className="navbar-brand mb-0 h1 ms-3">React Flask Boilerplate</span>
+            </Link>
+            <div className="ml-auto">
+                {!store.token ? ( 
+                    <>
+                        <Link to="/signup">
+                            <button className="btn btn-primary me-2">Registro</button>
+                        </Link>
+                        <Link to="/login">
+                            <button className="btn btn-success me-2">Iniciar Sesión</button>
+                        </Link>
+                    </>
+                ) : ( 
+                    <button onClick={handleLogout} className="btn btn-danger me-2">
+                        Cerrar Sesión
+                    </button>
+                )}
+            </div>
+        </nav>
+    );
 };
